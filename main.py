63ba0f07663206e9aa286f4a5927db572d160eaf
postmodernweaboo <raider824477@gmail.com>
2025-01-23 13:29:01 +0900

    Added features: Get; Character Dictionary, Word Count & Book Text.

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = get_word_count(text)
+    char_dic = get_char_dict(text)
+
+    print(book_path)
+
+
+
+def get_char_dict(text):
+    dict = {}
+    lowercase = text.lower()
+    for letter in lowercase:
+        if letter in dict:
+            dict[letter] += 1
+        else:
+            dict[letter] = 1
+    return dict
+
+
+def get_word_count(text):
+    words = len(text.split())
+    return words
+
+
+
+def get_book_text(book_path):
+    with open(book_path) as t:
+        return t.read()
+
 
 main()
