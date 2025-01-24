39dd0bf2045bc538e608227b71ced3a12ddc123d
postmodernweaboo <raider824477@gmail.com>
2025-01-24 13:48:16 +0900

    Added method to sort char dict and print a final report

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 def main():
+
+    #Where is the book located
     book_path = "books/frankenstein.txt"
+
+    #Functions of the report
     text = get_book_text(book_path)
     word_count = get_word_count(text)
-    char_dic = get_char_dict(text)
+    char_dict = get_char_dict(text)
+    sorted_dict = get_sorted_dict(char_dict)
 
-    print(book_path)
+    #Printing the report
+    print(f"Report for {book_path} is here!")
+    print(f"There were {word_count} words found in this book! WOW...\n")
+    
+    for x in sorted_dict:
+        print(f"'{x['char']}' was found '{x['freq']}' times...")
 
+    print(f"\nThanks for reading this report on {book_path} :)")
+
+#---------FUNCTIONS---------#
+
+def get_sorted_dict(char_dict):
+    char_freq = []
+    for c in char_dict:
+        if c.isalpha() == True:
+            char_freq.append({"char": c, "freq": char_dict[c]})
+    char_freq.sort(reverse=True, key=lambda x: x["freq"])
+    return char_freq
 
 
 def get_char_dict(text):
@@ -24,10 +45,8 @@ def get_word_count(text):
     return words
 
 
-
 def get_book_text(book_path):
     with open(book_path) as t:
         return t.read()
-
 
 main()
